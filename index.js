bfdbb272ce5cc46c393c1d8895403e8689fb43a5
jiayiming <jiayiming@innobuddy.com>
2018-05-25 11:41:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,54 @@
+// 文件上传处理中间件
+// 支持多文件上传，同名文件后者覆盖前者
+// ctx.args.fields
+// ctx.args.files
+const fs = require('fs');
+const formidable = require('formidable');
+
+function multipartHandle(ctx, form) {
+  return new Promise((resolve, reject) => {
+    form.parse(ctx.req, function (err, fields, files) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({
+          fields,
+          files
+        });
+      }
+    });
+  });
+}
+
+module.exports = function (options = {}) {
+
+  const defaults = {
+    uploadDir: undefined,
+    autoDelete: false,
+    maxFileSize: 200 * 1024 * 1024
+  }
+
+  options = Object.assign(defaults, options);
+
+  return async (ctx, next) => {
+    const form = new formidable.IncomingForm();
+
+    if (defaults.uploadDir) {
+      form.uploadDir = defaults.uploadDir;
+    }
+
+    form.maxFileSize = defaults.maxFileSize;
+
+    const args = await multipartHandle(ctx, form);
+    ctx = Object.assign(ctx, args);
+
+    await next();
+
+    if (options.autoDelete) {
+      for (let key in args.files) {
+        console.log(args.files[key].path);
+        fs.unlinkSync(args.files[key].path);
+      }
+    }
+  }
+}
