31988f2f85a9d8fe8e172e16aff6949e391cbb21
Jia Yiming <jiayiming@innobuddy.com>
2019-05-31 14:14:41 +0800

    axios 0.19.0

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const fs = require('fs');
 const formidable = require('formidable');
 const Readable = require('stream').Readable;
 
+function getFileName(dir, name, ext, index = 0) {
+  let result;
+  let filename = `${name}${index ? `(${index})` : ''}${ext}`;
+  try {
+    fs.accessSync(`${dir}/${filename}`);
+    result = getFileName(dir, name, ext, ++index);
+  } catch (err) {
+    result = filename
+  }
+  return result;
+}
+
 function multipartHandle(ctx, form, options) {
   return new Promise((resolve, reject) => {
-
     let streams = {};
-
-    form.parse(ctx.req, function (err, fields, files) {
+    form.parse(ctx.req, (err, fields, files) => {
       if (err) {
         reject(err);
       } else {
         if (options.stream) {
           resolve({
             fields,
-            streams
+            files: streams
           });
         } else {
           resolve({
@@ -25,24 +35,37 @@ function multipartHandle(ctx, form, options) {
       }
     });
 
-    form.onPart = function (part) {
+    form.on('file', (name, file) => {
+      //rename the incoming file to the file's name
+      let pointIndex = file.name.lastIndexOf('.');
+
+      file.name = getFileName(form.uploadDir, file.name.substring(0, pointIndex), file.name.substring(pointIndex));
+      fs.renameSync(file.path, form.uploadDir + "/" + file.name);
+      file.path = form.uploadDir + "/" + file.name;
+    });
+
+    form.onPart = (part) => {
       if (!options.stream) {
         form.handlePart(part);
       } else {
         if (!part.filename) {
           form.handlePart(part);
         } else {
-          streams[part.name] = new Readable();
-          streams[part.name]._read = function () {};
-          streams[part.name].filename = part.filename;
-          streams[part.name].type = part.mime;
+          streams[part.name] = {
+            stream: new Readable(),
+            name: part.filename,
+            type: part.mime,
+            size: 0
+          }
+          streams[part.name].stream._read = function () {};
 
-          part.on('data', function (chunk) {
-            streams[part.name].push(chunk);
+          part.on('data', (chunk) => {
+            streams[part.name].size += chunk.length;
+            streams[part.name].stream.push(chunk);
           });
 
-          part.on('end', function () {
-            streams[part.name].push(null);
+          part.on('end', () => {
+            streams[part.name].stream.push(null);
           });
         }
       }
@@ -60,6 +83,12 @@ module.exports = function (options = {}) {
   }
 
   options = Object.assign(defaults, options);
+
+  if (typeof options.uploadDir === 'string') {
+    if (options.uploadDir.charAt(options.uploadDir.length - 1) === '/') {
+      options.uploadDir = options.uploadDir.substr(0, options.uploadDir.length - 1);
+    }
+  }
 
   return async (ctx, next) => {
     const form = new formidable.IncomingForm();
